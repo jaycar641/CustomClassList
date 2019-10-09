ce7c5979afadd381f55cef139cebd4557faa906e
Jacob Carson <jaycar641@yahoo.com>
2019-10-08 20:06:59 -0500

    Initial Commit

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class CustomList<T>
+    {
+
+        Array[] distArray = new Array[4];
+
+        public void Add()
+        {
+
+        }
+
+        public void Remove()
+        {
+
+
+        }
+
+       
+
+    }
+}
---
+++ ListTest/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace ListTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_Object_IsIndexed()
+
+        {
+
+        }
+        [TestMethod]
+        public void Add_WrongObjectType_ReturnException()
+
+        {
+        }
+
+        [TestMethod]
+        public void Add_Object_CountIncreases()
+        {
+
+        }
+
+        [TestMethod]
+        public void Add_NullArgument_ReturnException()
+        {
+
+        }
+
+        [TestMethod]
+        public void Remove_Object_DecreasesCount()
+        {
+
+        }
+
+        [TestMethod]
+        public void Remove_Object_ReturnsTrue()
+        {
+
+        }
+
+        [TestMethod]
+        public void Remove_Object_ShiftsArrayElements()
+        {
+
+        }
+
+        [TestMethod]
+        public void Send_ObjectToString_ReturnsString()
+        {
+
+        }
+
+        [TestMethod]
+        public void Send_NoObjectToString_ReturnsNull()
+        {
+
+        }
+
+        [TestMethod]
+        public void Send_NullTypeToString_ReturnsNull()
+        {
+
+        }
+
+
+    }
+}
