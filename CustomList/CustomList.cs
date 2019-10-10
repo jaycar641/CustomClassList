950630f367b9fb37891f7cbc357fe37406ed6505
Jacob Carson <jaycar641@yahoo.com>
2019-10-10 16:46:41 -0500

    Added add method unit tests

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -4,25 +4,138 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CustomList
+namespace CustomClassList
 {
-    class CustomList<T>
+    public class CustomList<T>
     {
+        //Fields
+        private T[] listArray = new T[4];
+        private int itemCount = 0;
+        private int capacity;
+        
+        //Constructor/s
+        public CustomList()
+        {
+            Capacity = ListArray.Length;
 
-        Array[] distArray = new Array[4];
+        }
 
-        public void Add()
+
+        //INDEXER
+        public T this[int index]
+        {
+            get {
+
+
+                return listArray[index];
+            }
+            set {
+
+                value = listArray[index];
+            }
+        }
+
+        //Properties
+
+        public T[] ListArray
+        {
+            get
+            {
+                return listArray;
+            }
+
+            set
+            {
+                listArray = value;
+            }
+
+        }
+
+        public int ItemCount
+        {
+            get
+            {
+                return itemCount;
+            }
+
+            set
+            {
+                itemCount = value;
+            }
+
+        }
+
+        public int Capacity 
+        {
+            get
+            {
+                return capacity;
+            }
+            
+            set
+            {
+                capacity = value;
+            }
+
+
+        }
+
+
+     
+       //Functions
+        public void Add(T input)
+        {
+            if (ItemCount != Capacity)
+            {
+                listArray[ItemCount] = input;
+                ItemCount++;
+
+            }
+
+
+            else
+            {
+                if (ItemCount == Capacity)
+                {
+                    Capacity += 4;
+                }
+            }
+
+
+        }
+
+        public void Remove(T input)
+        {
+            if (ListArray.Contains(input))
+            {
+               
+            }
+
+        }
+
+        public void Zip()
         {
 
         }
 
-        public void Remove()
+        public void CheckCapacity ()
         {
-
+            
+                   // T[] listArrayReplace = new T[ListArray.Length + 4];
+                    
+                   // ListArray = null;
+                  
+                  //  ListArray = listArrayReplace;
 
         }
+
+            
+
+    }
+
+
 
        
 
-    }
+    
 }
--- ListTest/UnitTest1.cs
+++ ListTest/UnitTest1.cs
@@ -1,40 +1,131 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CustomList;
+using CustomClassList;
 
 namespace ListTest
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void Add_Object_IsIndexed()
+       
 
-        {
-
-        }
-        [TestMethod]
-        public void Add_WrongObjectType_ReturnException()
-
-        {
-        }
 
         [TestMethod]
         public void Add_Object_CountIncreases()
         {
+            //arrange
+            CustomList<int> testClass = new CustomList<int>();
 
+            int test1 = 5;
+            int expectedResult = 1;
+            int actualResults;
+
+            //act
+            testClass.Add(test1);
+            actualResults = testClass.ItemCount;
+            //assert
+            Assert.AreEqual(actualResults, expectedResult);
         }
 
         [TestMethod]
-        public void Add_NullArgument_ReturnException()
+        public void Add_Object_ListReturnsValue()
         {
+            //arrange
+            CustomList<int> testClass = new CustomList<int>();
+            int test1 = 5;
+            int expectedResult = test1;
+            int actualResults;
 
+            //act
+            testClass.Add(test1);
+            actualResults = testClass.ListArray[0];
+            //assert
+            Assert.AreEqual(actualResults, expectedResult);
         }
+
+        [TestMethod]
+        public void Add_Object_AddedAtNewIndex()
+        {
+            //arrange
+            CustomList<int> testClass = new CustomList<int>();
+            int test1 = 5;
+            int test2 = 3;
+            int testindex = 1;
+
+            int expectedResult = test2;
+            int actualResults;
+
+            //act
+            testClass.Add(test1);
+            testClass.Add(test2);
+            actualResults = testClass[1];
+            //assert
+            Assert.AreEqual(actualResults, expectedResult);
+        }
+
+
+        [TestMethod]
+        public void Add_ObjectCountIsEqualCapacity_ReturnsNewCapacity()
+        {
+            //arrange
+            CustomList<int> testClass = new CustomList<int>();
+            int expectedResults = 8;
+            int actualResults;
+
+
+            //act
+            testClass.Add(4);
+            testClass.Add(4);
+            testClass.Add(4);
+            testClass.Add(4);
+            testClass.Add(4);
+            actualResults = testClass.Capacity;
+
+            //assert
+            Assert.AreEqual(actualResults, expectedResults);
+        }
+
+
+             [TestMethod]
+        public void Add_ObjectCountIsEqualCapacity_ValuesInOldArrayReturnsInNewArray()
+        {
+            //arrange
+            CustomList<int> testClass = new CustomList<int>();
+            int actualResults;
+
+
+            //act
+            testClass.Add(4);
+            testClass.Add(8);
+            testClass.Add(12);
+            testClass.Add(16);
+            testClass.Add(20);
+            actualResults = testClass.Capacity;
+           
+            
+            actualResults = testClass.ListArray[testClass.ItemCount - 1];
+
+            //assert
+            Assert.AreEqual(actualResults, expectedResults);
+        }
+
 
         [TestMethod]
         public void Remove_Object_DecreasesCount()
         {
+            //arrange
+            CustomList<int> testClass = new CustomList<int>();
+            int test1 = 5;
+            //testArray[0] = test1;
 
+          //  int expectedResult = testArray.Length;
+            int actualResults;
+
+            //act
+            testClass.Remove(test1);
+          //  actualResults = testArray.Length;
+            //assert
+          //  Assert.AreEqual(actualResults, expectedResult);
         }
 
         [TestMethod]
@@ -66,6 +157,31 @@ namespace ListTest
         {
 
         }
+
+        public void Index_SelectIndex_SetsIndex()
+        {
+
+        }
+
+        public void Index_SelectIndex_GetsIndex()
+        {
+
+        }
+
+        public void Index_SelectIndex_ReturnIndex()
+        {
+
+        }
+
+        public void Index_SelectIndex_ReturnsIndexOutOfRangeException()
+        {
+
+        }
+
+
+
+
+
 
 
     }
